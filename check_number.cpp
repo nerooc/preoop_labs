d279eb777f66df79a82830eaa9ff0a728261c538
Tomasz Gajda <tomaszgajdait@gmail.com>
2019-10-18 17:02:12 +0200

    probably final version

--- check_number.cpp
+++ check_number.cpp
@@ -11,10 +11,7 @@ char find_ctrl(const char *karta){
 	int suma = 0;
 	int suma1 = 0;
 	int suma2 = 0;
-
-	
-
-	int ctrl;
+	int ctrl = 0;
 		
 		if(x == 16){
 			for(int i = x - 1; i >= 0 ; i--){
@@ -36,7 +33,9 @@ char find_ctrl(const char *karta){
 					 }
 
 					 else{
+					 
 					 	suma1 += (karta[i] - '0')*2;
+					 	
 					 }
 			
 				}  
@@ -53,33 +52,69 @@ char find_ctrl(const char *karta){
 		else{
 			for(int i = 16-(16-x) - 1; i >= 0 ; i--){
 
-				if(((i+1)%2)!=0){
+				if(x%2 == 0){
+
+					if(((i+1)%2)!=0){
 				 
-					 if((karta[i] - '0')*2 >= 10){
+						 if((karta[i] - '0')*2 >= 10){
 
-					 	char *strtmp2;
-						strtmp2 = (char*)malloc(2*sizeof(char));
-						
-						int k = (karta[i] - '0')*2;
-						sprintf(strtmp2, "%d", k); 
-						suma1 += (strtmp2[0]-'0') + (strtmp2[1]-'0');
+						 	char *strtmp2;
+							strtmp2 = (char*)malloc(2*sizeof(char));
+							
+							int k = (karta[i] - '0')*2;
+							sprintf(strtmp2, "%d", k); 
+							suma1 += (strtmp2[0]-'0') + (strtmp2[1]-'0');
 
-						free(strtmp2);
+							free(strtmp2);
 
-					 }
+						 }
 
-					 else{
-					 	suma1 += (karta[i] - '0')*2;
-					 }
+						 else{
+						 	suma1 += (karta[i] - '0')*2;
+						 }
 			
-				}  
+					}  
 
-				else{	
+					else{	
 				 
-					 	suma2 += (karta[i] - '0');
+						 	suma2 += (karta[i] - '0');
 				
 
+					}
 				}
+				
+				else{
+
+					if((i%2)!=0){
+				 
+						 if((karta[i] - '0')*2 >= 10){
+
+						 	char *strtmp2;
+							strtmp2 = (char*)malloc(2*sizeof(char));
+							
+							int k = (karta[i] - '0')*2;
+							sprintf(strtmp2, "%d", k); 
+							suma1 += (strtmp2[0]-'0') + (strtmp2[1]-'0');
+
+							free(strtmp2);
+
+						 }
+
+						 else{
+						 	suma1 += (karta[i] - '0')*2;
+						 }
+			
+					}  
+
+					else{	
+				 
+						 	suma2 += (karta[i] - '0');
+				
+
+					}
+				}
+
+				
 			}
 			
 		}
